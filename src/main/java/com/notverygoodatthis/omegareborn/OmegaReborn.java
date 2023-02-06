5ce23d557dc31dce126b60040cff5807794610d9
Tonći Ujević <tonci.ujevic@gmail.com>
2023-02-06 20:37:26 +0100

    initial commit. oh god i have to rewrite everythi-

---
+++ src/main/java/com/notverygoodatthis/omegareborn/OmegaReborn.java
@@ -0,0 +1,17 @@
+package com.notverygoodatthis.omegareborn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OmegaReborn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
